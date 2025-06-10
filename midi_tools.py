57e7043ae42c8384c95028ddd94f57dc0666513c
Conor Sheridan <c_sheridan@mailbox.org>
2025-06-10 15:15:48 +0100

    Update midi_tools.py
    redid scripts to add more options

--- midi_tools.py
+++ midi_tools.py
@@ -3,46 +3,6 @@ from mido import MidiFile
 import numpy as np
 from note_dict import note2num, num2note, hex2num
 
-# legend: 0,1,2,3,4,5,6,7,8,9,a,b = mod12 notes in hex format
-#         c                       = add a pause of 1 beat
-
-#takes list of (one or more) melody strings and concatonates it into a single MIDI file
-def midi_gen(mel_list,name):
-    mf = MIDIFile(1)     # one tracks
-    track = 0           # the only track
-
-    time = 0    # start at the beginning
-    mf.addTrackName(track, time, name)
-    mf.addTempo(track, time, 120)
-
-    volume = 100
-
-    # iterative beat value
-    b = 0
-    increment = 1 # amount to increment by
-
-    for j in range(len(mel_list)):
-        for i in mel_list[j]:
-
-            #allows break in tune
-            if hex2num[i] == 12:
-                b += 1
-            
-            else:
-                pitch = hex2num[i] + 60     # note converted from our format to MIDI format (+ 60)
-                time = b                    # start on beat b
-                duration = 1                # 1 beat long
-                mf.addNote(track, j, pitch, time, duration, volume)
-
-                b += increment # update beat by a given increment
-        
-        b = 0 # reset beat
-
-    # write it to disk
-    with open("{0}.mid".format(name), 'wb') as outf:
-        mf.writeFile(outf)
-
-
 
 
 # read a MIDI file and return a numpy array containing the melody
@@ -51,43 +11,55 @@ def midi_read(filename):
     #read MIDI file
     mf = MidiFile(filename)
 
-    # melody string
-    mldy_arry = []
+    # melody arrays
+    mldy_n = []     #notes
+    mldy_t = []     #times
 
     for msg in mf:
         if msg.type == "note_on":
-            mldy_arry.append(msg.note)
+            mldy_n.append(msg.note)
+            mldy_t.append(msg.time)
+        if msg.type == "set_tempo":
+            tempo = msg.tempo
     
-    return mldy_arry
+    return mldy_n, mldy_t, tempo
 
 
 
 # takes a melody array from midi_read and writes to a file
-def midi_write(mldy_arry,output):
+def midi_write(mldy_n_ar,mldy_d_ar,output,tempo=120,vol=True):
     
     mf = MIDIFile(1)     # one tracks
     track = 0           # the only track
 
     time = 0    # start at the beginning
     mf.addTrackName(track, time, output)
-    mf.addTempo(track, time, 120)
+    mf.addTempo(track, time, tempo)
 
-    volume = 100
+    channels = len(mldy_n_ar) #no. of channels
 
-    # iterative beat value
-    b = 0
-    increment = 1 # amount to increment by
 
-    for j in range(len(mldy_arry)):
-        for i in mldy_arry[j]:
-            pitch = i    # note converted from our format to MIDI format (+ 60)
-            time = b                    # start on beat b
-            duration = 1                # 1 beat long
-            mf.addNote(track, j, pitch, time, duration, volume)
+    # default is all channels have volume 100
+    if vol == True:
+        vol = np.zeros(channels)
+        vol += 100
 
-            b += increment # update beat by a given increment
+    T = len(mldy_d_ar)
+    timestmps = np.zeros(T)
+    
+    # generate note timestamps from durations
+    for d in range(T):
+        if d < T-1:
+            timestmps[d+1] = timestmps[d] + mldy_d_ar[d]
+        else:
+            pass
 
-        b = 0 # reset beat counter
+    # add notes per each channel
+    for c in range(channels):
+        for i in range(len(mldy_n_ar[c])):
+            pitch = mldy_n_ar[c][i]       # pitch from melody list
+            
+            mf.addNote(track, c, pitch, timestmps[i], mldy_d_ar[i], vol[c])
 
     # write it to disk
     with open("{0}.mid".format(output), 'wb') as outfile:
